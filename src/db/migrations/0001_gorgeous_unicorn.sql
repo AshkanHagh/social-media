967b661386322224e175c71aee4ed8daf765722f
Ashkan <AshkanHagh01@gmail.com>
2024-06-08 09:59:17 +0330

    Fix & Update

--- src/db/migrations/0001_gorgeous_unicorn.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE "replies" ADD COLUMN "authorId" uuid;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "replies" ADD CONSTRAINT "replies_authorId_users_id_fk" FOREIGN KEY ("authorId") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "indexCommentId" ON "replies" ("commentId");--> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "indexAuthorId" ON "replies" ("authorId");
